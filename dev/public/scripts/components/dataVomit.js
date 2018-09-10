3d57f55950255a01af368ea22e674de85fbf33b0
Ben Vasko <BenVasko@Bens-MacBook-Pro.local>
2018-09-10 14:18:13 -0400

    epic progress on switching between maps

--- dev/public/scripts/components/dataVomit.js
+++ dev/public/scripts/components/dataVomit.js
@@ -16,9 +16,25 @@ const data = {
         vm.getData();
 
         document.addEventListener("click", (e) => {
-            console.log(e.target);
-            // angular.element(e.target).addClass("red");
+            console.log(e.target.innerHTML);
+            if (angular.element(e.target).attr("class")){
+                vm.stateID = (angular.element(e.target).attr("class").slice(-2)); 
+            } else if (e.target.innerHTML) {
+                vm.stateID = e.target.innerHTML;
+            }
+                let ak1 = document.createElement("script");
+                    ak1.type = "text/javascript";
+                    ak1.src = `scripts/states/${vm.stateID}/mapdata.js`
+                    ak1.innerHTML = null;
+                    document.getElementById("map-scripts").innerHTML = "";
+                    document.getElementById("map-scripts").appendChild(ak1);
+                let ak2 = document.createElement("script");
+                    ak2.type = "text/javascript";
+                    ak2.src = `scripts/states/${vm.stateID}/statemap.js`;
+                    ak2.innerHTML = null;
+                    document.getElementById("map-scripts").appendChild(ak2);
         });
+
     }]
 };
 
